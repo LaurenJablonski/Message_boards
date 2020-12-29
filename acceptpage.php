79a06f55897d1194c78bbdc8a32dc4a7e4746513
LaurenJablonski <jablonski.lauren1@gmail.com>
2020-12-29 14:26:56 +0000

    added button to discard message as well as an alert to appear on the page when the user tries to post a message but hasn't written anything

---
+++ acceptpage.php
@@ -0,0 +1,6 @@
+<?php
+$frominput = $_POST[input]; // Variable who accept your data from input
+$fromtextarea = $_POST[text]; // Variable who accept your data from textarea
+echo $fromtextarea;
+echo $frominput;
+?>
--- message_script.js
+++ message_script.js
@@ -1,3 +1,24 @@
 
+function postMessage() {
+
+    let messageEntered = document.getElementById("user-inputs-new-message").value;//gets the data entered by the user
 
 
+
+    if (messageEntered == "" || messageEntered == null) {// series for statements that give alerts if the user has forgotten to enter the name. description or task
+        alert("Please enter a message!");
+    } else {
+        console.log("thanks for submitting a message");
+        // addItem(todoItemname, todoItemDesc, todoItemDate);
+        // clearAndRefresh();//if name, description and due date have been added then clear the form and refresh the page.
+
+    }
+}
+
+
+var input = document.querySelector('#clear_message_button');
+var textarea = document.querySelector('#user-inputs-new-message');
+
+input.addEventListener('click', function () {
+    textarea.value = '';
+}, false);
